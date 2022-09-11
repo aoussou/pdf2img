627c14c02f84b62a6edc8cadc796eb1a7c3ce781
ceo <john@talisol.com>
2022-09-11 13:02:05 +0900

    add one more pre0 to numbers

--- test_pypdfium2.py
+++ test_pypdfium2.py
@@ -6,16 +6,22 @@ This is a temporary script file.
 """
 
 import pypdfium2 as pdfium
+import os
+
+def create_dir(path):
+    if not os.path.isdir(path):
+        os.makedirs(path)
 
 # Load a document
-filepath = "新にほんご500問　N2.pdf"
+book_name = "新にほんご500問　N2"
+filepath = os.path.join("..","..","data","books", book_name + ".pdf")
 pdf = pdfium.PdfDocument(filepath)
 
-# render a single page (in this case: the first one)
-page = pdf.get_page(0)
-pil_image = page.render_topil(scale=2)
-pil_image.save("output.jpg")
-page.close()
+save_dir = os.path.join("..","..","data","book_images", book_name)
+
+create_dir(save_dir)
+
+
 
 # render multiple pages concurrently (in this case: all)
 page_indices = [i for i in range(len(pdf))]
@@ -24,7 +30,10 @@ renderer = pdf.render_topil(
     scale=3
 )
 for image, index in zip(renderer, page_indices):
-    image.save("output/%02d.jpg" % index)
+    
+    save_path = os.path.join(save_dir,"%03d.jpg" % index)
+    
+    image.save(save_path)
     image.close()
 
 pdf.close()
